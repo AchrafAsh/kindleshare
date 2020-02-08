51fd1070159f6b0019ce6c7147bed15bc1fd33d1
Ait sidi hammou Achraf <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-02-08 15:16:50 +0100

    Build

---
+++ client/build/precache-manifest.2147908065d9196887349c405420efbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff6089c8377adc7864c03716625a8c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36bf4ae2e401c076a0c6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d252f0259fa1ed7f0f02",
+    "url": "/static/css/main.8020e407.chunk.css"
+  },
+  {
+    "revision": "36bf4ae2e401c076a0c6",
+    "url": "/static/js/2.cf4457a6.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/static/js/2.cf4457a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d252f0259fa1ed7f0f02",
+    "url": "/static/js/main.730eb7cf.chunk.js"
+  },
+  {
+    "revision": "65326020070271871721",
+    "url": "/static/js/runtime-main.2df13cd9.js"
+  }
+]);
